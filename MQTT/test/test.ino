2237496b304afd9fa780dbeac79e992e03dccefd
Felipe-noob <felipelorenzzon@gmail.com>
2024-11-27 08:49:02 +0100

    fixed timeout

--- MQTT/test/test.ino
+++ MQTT/test/test.ino
@@ -5,16 +5,12 @@
  *
  * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-mqtt
  */
-
 /*
   Complete Getting Started Guide: https://RandomNerdTutorials.com/esp32-bluetooth-low-energy-ble-arduino-ide/
   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
   Ported to Arduino ESP32 by Evandro Copercini
 */
 
-
-#include <BLEDevice.h>
-#include <BLEServer.h>
 #include <WiFi.h>
 #include <MQTTClient.h>
 #include <string.h>
@@ -30,53 +26,10 @@ const char MQTT_PASSWORD[] = "";  // CHANGE IT IF REQUIRED
 
 // The MQTT topics that this device should publish/subscribe
 #define PUBLISH_TOPIC "esp32-001/send"
-#define SUBSCRIBE_TOPIC "esp32-001/send"
-#define PUBLISH_INTERVAL 5000  // 4 seconds
 
 WiFiClient network;
 MQTTClient mqtt = MQTTClient(256);
-unsigned long lastPublishTime = 0;
 String message;
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
-#define SERVICE_UUID        "2658a61b-6efd-4760-9f41-0e8489222429"
-#define CHARACTERISTIC_UUID "ef9b96c4-c17f-45cb-bc57-0eefeaff93fd"
-
-BLECharacteristic *pCharacteristic;
-
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      String value = pCharacteristic->getValue();
-
-      uint32_t int_val = (uint32_t) value.c_str();
-
-      Serial.println("some value arrived");
-      delay(100);
-      Serial.println(value);
-      delay(100);
-      print_binary(int_val);
-    }
-
-    
-    bool get_bit(uint32_t num, uint32_t position)
-    {
-      bool bit = num & (1 << position);
-      return bit;
-    }
-    
-    
-    void print_binary(uint32_t num)
-    {
-      Serial.print("binary: ");
-      for(int i = 31; i >= 0; i--)
-      {
-        Serial.print(get_bit(num, i));
-      }
-      Serial.println(" -- ");
-    }
-    
-};
 
 void setupWifi() {
   WiFi.mode(WIFI_STA);
@@ -84,7 +37,7 @@ void setupWifi() {
 
   Serial.println("ESP32 - Connecting to Wi-Fi");
  
-  rgbLedWrite(RGB_BUILTIN, 64, 0, 0);
+  rgbLedWrite(RGB_BUILTIN, 64, 64, 0);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -93,33 +46,10 @@ void setupWifi() {
   rgbLedWrite(RGB_BUILTIN, 0, 0, 64);
 }
 
-void setupBT() {
-  BLEDevice::init("LorenzzonGato");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
-  pCharacteristic->setCallbacks(new MyCallbacks()); 
-  pCharacteristic->setValue("hola");
-  pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
-  BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
-}
-
 void setup() {
-  
   // recieves from serial from another ESP32
   Serial1.begin(115200, SERIAL_8N1, 16, 17);
+  // For debugging
   Serial.begin(115200);
 
   setupWifi();
@@ -131,22 +61,9 @@ void setup() {
 }
 
 void loop() {
-  // mqtt.loop();
-
-  // if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
-  //   sendToMQTT();
-  //   lastPublishTime = millis();
-  // }
-  //Serial.readBytes(Mymessage,5); //Read the //Serial data and store in var
-  //Serial.println(Mymessage); //Print data on //Serial Monitor
-  // Serial.println("Loop");
   if (Serial1.available()) {
-
-    message = Serial1.readString();
-    
-    Serial.println("Recebi a mensagem seguinte:");
-    Serial.println(message);
-    connectToMQTT();
+    message = Serial1.readStringUntil('\n');
+    Serial.println("Recieved: " + message);
     sendToMQTT();
   }
 }
@@ -155,9 +72,7 @@ void loop() {
 void connectToMQTT() {
   // Connect to the MQTT broker
   mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
-
-  // Create a handler for incoming messages
-  // mqtt.onMessage(messageHandler);
+  mqtt.setTimeout(30000);
 
   Serial.print("ESP32 - Connecting to MQTT broker");
 
@@ -165,34 +80,21 @@ void connectToMQTT() {
     Serial.print(".");
     delay(100);
   }
-  Serial.println();
 
   if (!mqtt.connected()) {
     Serial.println("ESP32 - MQTT broker Timeout!");
+    rgbLedWrite(RGB_BUILTIN, 64, 0, 0);
     return;
   }
-
-  // Subscribe to a topic, the incoming messages are processed by messageHandler() function
-  // if (mqtt.subscribe(SUBSCRIBE_TOPIC)) {
-    //Serial.print("ESP32 - Subscribed to the topic: ");
-    // rgbLedWrite(RGB_BUILTIN, 0, 64, 0);
-  // } else {
-    //Serial.print("ESP32 - Failed to subscribe to the topic: ");
-    // rgbLedWrite(RGB_BUILTIN, 64, 0, 0);
-  // }
-  //Serial.println(SUBSCRIBE_TOPIC);
-  //Serial.println("ESP32  - MQTT broker Connected!");
+  Serial.println("\nConnected");
+  rgbLedWrite(RGB_BUILTIN, 0, 64, 0);
 }
-
 
 void sendToMQTT() {
+  if (!mqtt.connected()) {
+    Serial.println("Not connected!");
+    connectToMQTT();
+    rgbLedWrite(RGB_BUILTIN, 0, 0, 64);
+  }
   mqtt.publish(PUBLISH_TOPIC, message);
-}
-
-
-void messageHandler(String &topic, String &payload) {
-  Serial.println("ESP32 - received from MQTT:");
-  Serial.println("- topic: " + topic);
-  Serial.println("- payload:");
-  Serial.println(payload);
 }
